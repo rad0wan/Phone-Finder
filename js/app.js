7c2614b42b8bc85566a7ce260f615fcd90674eb2
unknown <radowan.me@gmail.com>
2022-02-28 22:52:41 +0600

    load card

--- js/app.js
+++ js/app.js
@@ -7,5 +7,30 @@ const searchPhone = () => {
         .then(data => loadPhone(data.data));
 }
 const loadPhone = (phones) => {
-    phones.forEach(phone => console.log(phone))
+    const phonesDiv = document.getElementById('phones');
+    phones.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top " alt="">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+                <button onclick="loadDetails('${phone.slug}')" type="button" class="btn btn-success ms-5 px-3">Details</button> 
+            </div>
+        </div>
+        `
+        phonesDiv.appendChild(div);
+    })
+}
+const loadDetails = (phoneId) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data.data))
+}
+const displayDetails = (Details) => {
+  console.log(Details);
 }
