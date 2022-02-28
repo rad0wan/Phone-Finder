95693248770163011c1e6efeab4cbef5a40e907b
unknown <radowan.me@gmail.com>
2022-02-28 22:07:25 +0600

    loadPhone

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('hmmm');
+const searchPhone = () => {
+    const searchFeild = document.getElementById('search-feild');
+    const searchFeildValue = searchFeild.value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchFeildValue}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loadPhone(data.data));
+}
+const loadPhone = (phones) => {
+    phones.forEach(phone => console.log(phone))
+}
